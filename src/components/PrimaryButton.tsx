c02d841a835704ee3edb6f99211d347221782719
TokolohoMohale <150892343+TokolohoMohale@users.noreply.github.com>
2024-09-29 14:34:58 +0200

    feat: added "PimaryButton" component

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface Props {
+  label: string;
+  onClick: any;
+  icon: any;
+}
+
+function PrimaryButton({ label, onClick, icon }: Props) {
+  return (
+    <button
+      className="bg-[#93C23D] flex items-center justify-center gap-2 py-[5px] rounded-md border border-[#A5FF00] hover:bg-gradient-to-br hover:from-[#93C23D] hover:to-[#B8E5DE] hover:cursor-pointer transition-all delay-150 ease-in-out duration-150"
+      onClick={onClick}
+    >
+      {icon}
+      <h4 className="font-lato font-normal text-[16px] text-white hidden md:block">
+        {label}
+      </h4>
+    </button>
+  );
+}
+
+export default PrimaryButton;
