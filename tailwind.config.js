e2192d516b3b6610ea53d8aa2553cc9062039f28
TokolohoMohale <150892343+TokolohoMohale@users.noreply.github.com>
2024-09-29 14:29:27 +0200

    chore: screen settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      // sm: '480px',
+      // md: '768px',
+      // lg: '976px',
+      // xl: '1440px',
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
     extend: {
       colors: {
         primary: "#93C23D",
