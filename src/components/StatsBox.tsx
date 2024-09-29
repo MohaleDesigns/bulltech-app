ee6a95799661fb92bb9cf3c009a281826fb3b9cf
TokolohoMohale <150892343+TokolohoMohale@users.noreply.github.com>
2024-09-29 14:34:15 +0200

    feat: added "StatsPanel" component

---
+++ src/components/StatsBox.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface Props {
+  label: string;
+  number: number;
+  icon: any;
+}
+
+function StatsBox({ label, number, icon }: Props) {
+  return (
+    <div className="flex items-center justify-between p-3 rounded-md bg-gradient-to-br from-[#93C23D] to-[#B8E5DE]">
+      <div>
+        <h4 className="font-overpass text-[16px] font-medium text-white">
+          {label}
+        </h4>
+        <p className="font-lato text-[15px] font-normal text-white">{number}</p>
+      </div>
+
+      <div className="flex items-center justify-center p-3 rounded-md bg-white">
+        {icon}
+      </div>
+    </div>
+  );
+}
+
+export default StatsBox;
